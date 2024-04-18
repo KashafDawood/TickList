640dc8cff613a4402dfb27262d0e0ceec9d57adf
kashafdawood <kashaf.dawood@gmail.com>
2024-04-18 22:10:09 +0500

    eslint and database setup

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require("express");
+/* eslint-disable import/no-extraneous-dependencies */
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from ticklist app");
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello from ticklist app');
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const app = require("./app");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const port = 3000;
-const server = app.listen(port, () => {
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//DATABASE CONNECTION
+mongoose.connect(DB).then(() => {
+  console.log('DB connected successfully ✅');
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
   console.log(`App is running on ${port}...`);
 });

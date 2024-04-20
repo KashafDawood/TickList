6a023a4a4229b3442c499abcd5726969a2899db4
kashafdawood <kashaf.dawood@gmail.com>
2024-04-20 20:54:51 +0500

    advance api filtering

--- controllers/factoryHandler.js
+++ controllers/factoryHandler.js
@@ -19,9 +19,12 @@ exports.findAll = Model =>
     // excluded feilds that we will implement later
     const excludeFeilds = ['sort', 'feilds', 'limit', 'page'];
     excludeFeilds.forEach(el => delete queryObj[el]);
-    console.log(req.query, queryObj);
 
-    const docs = await Model.find(queryObj);
+    // converting the queryObj to string so we can apply replace function to add $ with (gt|gte|lt|lte)
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
+
+    const docs = await Model.find(JSON.parse(queryStr));
 
     res.status(200).json({
       status: 'success',

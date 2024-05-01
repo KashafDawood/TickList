50fc58edca1ab275c8e63e6bb87c996abd9c0186
kashafdawood <kashaf.dawood@gmail.com>
2024-05-01 20:37:43 +0500

    update user password

--- controllers/authController.js
+++ controllers/authController.js
@@ -179,3 +179,22 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // Log the user in, send JWT
   createSendToken(user, 200, res);
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // get the user from the collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // check if posted current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong!', 401));
+  }
+
+  // if so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findbyidandupdate will not work as intented!, validation will not run
+
+  // log user in and sent jwt
+  createSendToken(user, 200, res);
+});
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,6 +8,9 @@ router.route('/signup').post(authController.signUp);
 router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').post(authController.resetPassword);
+router
+  .route('/updatePassword')
+  .post(authController.protect, authController.updatePassword);
 
 router.route('/').get(authController.protect, userController.getAllUsers);
 

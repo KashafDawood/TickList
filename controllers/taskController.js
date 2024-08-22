1d803df36e399882766622dea70ba11fac71de15
kashafdawood <kashaf.dawood@gmail.com>
2024-08-22 18:46:43 +0500

    create a project task end point

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,9 +1,40 @@
 const Task = require('./../models/taskModel');
 const factoryHandler = require('./factoryHandler');
-// const catchAsync = require('./../utils/catchAsync');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.createTask = factoryHandler.createOne(Task);
 exports.getAllTasks = factoryHandler.findAll(Task);
 exports.getTask = factoryHandler.findOne(Task);
 exports.updateTask = factoryHandler.updateOne(Task);
 exports.deleteTask = factoryHandler.deleteOne(Task);
+
+exports.projectTask = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+  const projectId = req.body.project;
+  const taskAssignBy = req.body.assignBy;
+
+  if (!projectId) {
+    return next(new AppError('Project field cannot be null', 404));
+  }
+
+  if (!taskAssignBy) {
+    return next(new AppError('AssignBy feild cannot be null', 404));
+  }
+
+  const taskData = {
+    user: userId,
+    project: projectId,
+    assignBy: taskAssignBy,
+    ...req.body
+  };
+
+  const task = await Task.create(taskData);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      data: task
+    }
+  });
+});

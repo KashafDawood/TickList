e6dbb4e14a9fb7855597c4b4faad7997f39018b9
kashafdawood <kashaf.dawood@gmail.com>
2024-09-17 06:02:51 +0500

    fixing httponly cookie

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const authController = require('./../controllers/authController');
-const userController = require('../controllers/userController');
+const express = require("express");
+const authController = require("./../controllers/authController");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.route('/signup').post(authController.signUp);
-router.route('/login').post(authController.login);
-router.route('/forgotPassword').post(authController.forgotPassword);
-router.route('/resetPassword/:token').post(authController.resetPassword);
-router.route('/updateMe').post(authController.protect, userController.updateMe);
-router.route('/deleteMe').post(authController.protect, userController.deleteMe);
+router.route("/signup").post(authController.signUp);
+router.route("/login").post(authController.login);
+router.route("/forgotPassword").post(authController.forgotPassword);
+router.route("/resetPassword/:token").post(authController.resetPassword);
+router.route("/updateMe").post(authController.protect, userController.updateMe);
+router.route("/deleteMe").post(authController.protect, userController.deleteMe);
 router
-  .route('/updatePassword')
+  .route("/updatePassword")
   .post(authController.protect, authController.updatePassword);
 
 router
-  .route('/getUserProjects')
+  .route("/getUserProjects")
   .get(authController.protect, userController.getUserProjects);
 
-router.route('/').get(authController.protect, userController.getAllUsers);
+router.route("/").get(authController.protect, userController.getAllUsers);
 
 module.exports = router;

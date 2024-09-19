48fb43ee3ec281187e6a728586760d86dcd6ee1e
kashafdawood <kashaf.dawood@gmail.com>
2024-09-19 21:53:16 +0500

    add get user task endpoint

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,7 +1,7 @@
-const Task = require('./../models/taskModel');
-const factoryHandler = require('./factoryHandler');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('../utils/appError');
+const Task = require("./../models/taskModel");
+const factoryHandler = require("./factoryHandler");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.createTask = factoryHandler.createOne(Task);
 exports.getAllTasks = factoryHandler.findAll(Task);
@@ -15,26 +15,54 @@ exports.projectTask = catchAsync(async (req, res, next) => {
   const taskAssignBy = req.body.assignBy;
 
   if (!projectId) {
-    return next(new AppError('Project field cannot be null', 404));
+    return next(new AppError("Project field cannot be null", 404));
   }
 
   if (!taskAssignBy) {
-    return next(new AppError('AssignBy feild cannot be null', 404));
+    return next(new AppError("AssignBy feild cannot be null", 404));
   }
 
   const taskData = {
     user: userId,
     project: projectId,
     assignBy: taskAssignBy,
-    ...req.body
+    ...req.body,
   };
 
   const task = await Task.create(taskData);
 
   res.status(201).json({
-    status: 'success',
+    status: "success",
     data: {
-      data: task
-    }
+      data: task,
+    },
+  });
+});
+
+exports.getUserTasks = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+
+  const tasks = await Task.find({
+    user: userId,
+  })
+    .populate({
+      path: "user",
+      select: "name email",
+    })
+    .populate({
+      path: "project",
+      select: "name description",
+    });
+
+  if (tasks.length === 0) {
+    return next(new AppError("No tasks found for this user", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    results: tasks.length,
+    data: {
+      tasks,
+    },
   });
 });
--- routes/taskroute.js
+++ routes/taskroute.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const taskController = require('./../controllers/taskController');
-const authController = require('./../controllers/authController');
+const express = require("express");
+const taskController = require("./../controllers/taskController");
+const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
 router
-  .route('/projectTask')
+  .route("/projectTask")
   .post(authController.protect, taskController.projectTask);
 
 router
-  .route('/')
+  .route("/getUserTasks")
+  .get(authController.protect, taskController.getUserTasks);
+
+router
+  .route("/")
   .post(authController.protect, taskController.createTask)
   .get(taskController.getAllTasks);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(taskController.getTask)
   .patch(taskController.updateTask)
   .delete(taskController.deleteTask);

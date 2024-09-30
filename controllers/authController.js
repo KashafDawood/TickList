5d0cac5fc5075c199ff06da19e3404ed04dadfc9
kashafdawood <kashaf.dawood@gmail.com>
2024-09-30 07:15:59 +0500

    add logout endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,6 +72,14 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   // getting the token and check if it is there
   let token;
